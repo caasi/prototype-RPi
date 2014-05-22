7ce6d77c4f36a6114d1fc1f7aedaa70b4c3e6bf7
caasi Huang <caasi.igd@gmail.com>
2014-05-22 20:56:20 +0800

    first working empty window

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #/usr/bin/python
+import demo
 import pi3d
 
 DISPLAY = pi3d.Display.create(x=150, y=150)
+KEYS    = pi3d.Keyboard()
 
-DISPLAY.destroy()
+while DISPLAY.loop_running():
+    if KEYS.read() == 27:
+        KEYS.close()
+        DISPLAY.destroy()
