868ec889bb137b68ce7fccb8bd70a53772a2bf45
caasi Huang <caasi.igd@gmail.com>
2014-05-29 20:05:22 +0800

    use REST API to update the app

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 (function(){
-  console.log('foo');
+  var express, gift, bluebird, repo, currentCommit, remoteFetch, remotes, sync, x$, app;
+  express = require('express');
+  gift = require('gift');
+  bluebird = require('bluebird');
+  repo = gift("./");
+  currentCommit = bluebird.promisify(repo.current_commit, repo);
+  remoteFetch = bluebird.promisify(repo.remote_fetch, repo);
+  remotes = bluebird.promisify(repo.remotes, repo);
+  sync = bluebird.promisify(repo.sync, repo);
+  x$ = app = express();
+  x$.get('/version', function(req, res){
+    return currentCommit().then(function(commit){
+      return res.send(commit.id);
+    })['catch'](function(e){
+      return res.send(500, e);
+    });
+  });
+  x$.put('/version', function(req, res){
+    return remoteFetch('origin').then(function(){
+      return remotes().then(function(remotes){
+        return currentCommit().then(function(commit){
+          if (commit.id === remotes[0].commit.id) {
+            return res.send(304);
+          } else {
+            return sync().then(function(){
+              return res.send(remotes[0].commit.id);
+            });
+          }
+        });
+      });
+    })['catch'](function(e){
+      return res.send(500, e);
+    });
+  });
+  x$.listen(8888, function(){
+    return console.log("server started");
+  });
 }).call(this);
--- src/index.ls
+++ src/index.ls
@@ -1,1 +1,31 @@
-console.log \foo
+require! express
+require! gift
+require! bluebird
+
+repo = gift "./"
+current-commit = bluebird.promisify repo.current_commit, repo
+remote-fetch   = bluebird.promisify repo.remote_fetch, repo
+remotes        = bluebird.promisify repo.remotes, repo
+sync           = bluebird.promisify repo.sync, repo
+
+port = 8888
+
+(app = express!)
+  ..get '/version' (req, res) ->
+    current-commit!
+      .then (commit) -> res.send commit.id
+      .catch (e) -> res.send 500, e
+  ..put '/version' (req, res) ->
+    remote-fetch 'origin'
+      .then ->
+        remotes <- remotes!then
+        commit  <- current-commit!then
+        if commit.id is remotes.0.commit.id
+          res.send 304
+        else
+          sync!then ->
+            res.send remotes.0.commit.id
+            # should exit and run the new script
+            process.exit 1
+      .catch (e) -> res.send 500, e
+  ..listen port, -> console.log "listen on #{port}"
