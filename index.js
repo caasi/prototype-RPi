8838309bafb83896e1249b040242eded1a2e3eec
caasi Huang <caasi.igd@gmail.com>
2014-05-31 03:25:33 +0800

    log with winston

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 (function(){
-  var express, gift, bluebird, repo, currentCommit, remoteFetch, remotes, sync, port, x$, app;
+  var express, gift, bluebird, winston, logger, repo, currentCommit, remoteFetch, remotes, sync, possibleIp, port, x$, app;
   express = require('express');
   gift = require('gift');
   bluebird = require('bluebird');
+  winston = require('winston');
+  logger = new winston.Logger({
+    transports: [new winston.transports.Console({
+      colorize: true
+    })]
+  });
   repo = gift("./");
   currentCommit = bluebird.promisify(repo.current_commit, repo);
   remoteFetch = bluebird.promisify(repo.remote_fetch, repo);
   remotes = bluebird.promisify(repo.remotes, repo);
   sync = bluebird.promisify(repo.sync, repo);
+  possibleIp = function(req){
+    return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  };
   port = 8888;
   x$ = app = express();
   x$.get('/version', function(req, res){
+    logger.info("GET /version from " + possibleIp(req));
     return currentCommit().then(function(commit){
+      logger.info("SEND commit id: " + commit.id);
       return res.send(commit.id);
     })['catch'](function(e){
+      logger.error("cannot get current commit: " + e);
       return res.send(500, e);
     });
   });
   x$.put('/version', function(req, res){
+    logger.info("PUT /version from " + possibleIp(req));
     return remoteFetch('origin').then(function(){
       return remotes().then(function(remotes){
         return currentCommit().then(function(commit){
           if (commit.id === remotes[0].commit.id) {
+            logger.info("SEND not modified");
             return res.send(304);
           } else {
             return sync().then(function(){
+              logger.info("SEND commit id: " + remotes[0].commit.id + ", update itself and leave");
               res.send(remotes[0].commit.id);
               return process.exit(0);
             });
@@ -32,13 +47,15 @@
         });
       });
     })['catch'](function(e){
+      logger.error("cannot update itself: " + e);
       return res.send(500, e);
     });
   });
   x$.get('/focus', function(req, res){
+    logger.info("GET /focus from " + possibleIp(req));
     return res.send(void 8);
   });
   x$.listen(port, function(){
-    return console.log("listen on " + port);
+    return logger.info("APIs listen on " + port);
   });
 }).call(this);
--- src/index.ls
+++ src/index.ls
@@ -1,6 +1,12 @@
 require! express
 require! gift
 require! bluebird
+require! winston
+
+logger = new winston.Logger do
+  transports:
+    * new winston.transports.Console colorize: true
+    ...
 
 repo = gift "./"
 current-commit = bluebird.promisify repo.current_commit, repo
@@ -8,26 +14,40 @@ remote-fetch   = bluebird.promisify repo.remote_fetch, repo
 remotes        = bluebird.promisify repo.remotes, repo
 sync           = bluebird.promisify repo.sync, repo
 
+possible-ip = (req) ->
+  req.headers['x-forwarded-for'] or req.connection.remoteAddress
+
 port = 8888
 
 (app = express!)
   ..get '/version' (req, res) ->
+    logger.info "GET /version from #{possible-ip req}"
     current-commit!
-      .then (commit) -> res.send commit.id
-      .catch (e) -> res.send 500, e
+      .then (commit) ->
+        logger.info "SEND commit id: #{commit.id}"
+        res.send commit.id
+      .catch (e) ->
+        logger.error "cannot get current commit: #{e}"
+        res.send 500, e
   ..put '/version' (req, res) ->
+    logger.info "PUT /version from #{possible-ip req}"
     remote-fetch 'origin'
       .then ->
         remotes <- remotes!then
         commit  <- current-commit!then
         if commit.id is remotes.0.commit.id
+          logger.info "SEND not modified"
           res.send 304
         else
           sync!then ->
+            logger.info "SEND commit id: #{remotes.0.commit.id}, update itself and leave"
             res.send remotes.0.commit.id
             # should exit and run the new script
             process.exit 0
-      .catch (e) -> res.send 500, e
+      .catch (e) ->
+        logger.error "cannot update itself: #{e}"
+        res.send 500, e
   ..get '/focus' (req, res) ->
+    logger.info "GET /focus from #{possible-ip req}"
     res.send void
-  ..listen port, -> console.log "listen on #{port}"
+  ..listen port, -> logger.info "APIs listen on #{port}"
